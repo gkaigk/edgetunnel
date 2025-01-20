8da5f4ca42383d71b2111aab1abee07e7365b24f
gkaigk <gkaigk@gmail.com>
2025-01-20 14:27:36 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '64369f70-22d5-4668-83fa-9fd034672ba0';
 let proxyIP = '';
 //let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
